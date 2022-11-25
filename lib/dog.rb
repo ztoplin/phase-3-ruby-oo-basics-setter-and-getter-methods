99e7a2e5222c7f1c20dc99dcca01b4181ce4bc0d
Aaron <ztoplin@yahoo.com>
2022-11-25 15:58:22 -0500

    commit tree c60cb6403b68d3a47d229c7c0a76ce8ec8fd3dae

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
 
+    # setter method
+    def name=(name_value)
+        @name = name_value
+    end
+
+    # getter method
+     def name
+        @name
+     end
+
+     # setter method
+     def breed=(breed_value)
+        @breed = breed_value
+     end
+
+     # getter method
+     def breed
+        @breed
+     end
+
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+    # setter method
+    def name=(name_value)
+        @name = name_value
+    end
+
+    # getter method
+    def name
+        @name
+    end
+
+    # setter method
+    def job=(job_value)
+        @job = job_value
+    end
+
+    # getter method
+    def job
+        @job
+    end
+
 end
